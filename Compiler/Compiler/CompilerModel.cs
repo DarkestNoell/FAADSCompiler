bf0187513b90e5c61cfbf92e847e17c6744e7fb4
DESKTOP-N0550UE\Austin Fouty <foutyaustinj@altissian.com>
2017-01-19 23:30:07 -0700

    -CompilerModel.cs should hold all static data the compiler needs -Symbol Table is constructed like a binary tree (NOT balanced) -The nodes of the tree each contain a symbol table, left/right child, and parent node -Symbol table information is stored in a dictionary with a key as the table ID and the value as a Symbol object
    For a better understanding of the strucutre, if needed, vist:
    https://www.tutorialspoint.com/compiler_design/compiler_design_symbol_table.htm
    
    P.S. Jus10 is a fgt

---
+++ Compiler/Compiler/CompilerModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler
+{
+    public class CompilerModel
+    {
+        //Use this class to store static variables regarding the compiler
+        public static SymbolTableNode Head;
+    }
+}
---
+++ Compiler/Compiler/SymbolTableNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler
+{
+    public class SymbolTableNode
+    {
+        private SymbolTableNode Parent;
+        private SymbolTableNode RightChild;
+        private SymbolTableNode LeftChild;
+        private Dictionary<int, Symbol> SymbolTableData = new Dictionary<int, Symbol>();
+
+        public SymbolTableNode(SymbolTableNode parent, SymbolTableNode rightChild, SymbolTableNode leftChild)
+        {
+            this.Parent = parent;
+            this.RightChild = rightChild;
+            this.LeftChild = leftChild;
+        }
+    }
+}
