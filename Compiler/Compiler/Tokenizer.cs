2da0571245f77351caee052f4cd362fd8e3a06b9
Selina <selina744@gmail.com>
2017-01-24 09:39:16 -0700

    State machine almost finished. Just needs parse exceptions

--- Compiler/Compiler/Tokenizer.cs
+++ Compiler/Compiler/Tokenizer.cs
@@ -26,22 +26,62 @@ namespace Compiler
                         }
                         else if (IsSymbol(c))
                         {
+                            sb.Append(c);
                             state = EState.Symbol;
+                            if (instructions[i+1] == '=')
+                            {
+                                sb.Append(c);
+                            }
+                            Symbol token = new Symbol(sb.ToString(), state);
+                            tokens.Add(token);
+                            sb.Clear();
                         }
                         else if (IsLetter(c))
                         {
+                            sb.Append(c);
                             state = EState.Id;
                         }
                         else if (IsQuotation(c))
                         {
+                            sb.Append(c);
                             state = EState.String;
                         }else if (IsNum(c))
                         {
-                            
+                            sb.Append(c);
+                            state = EState.Num;
                         }
 
                         break;
                     case EState.Symbol:
+                        if (IsWhiteSpace(c))
+                        {
+                            //No need to make token here because symbol token is made above
+                            state = EState.Space;
+                            sb.Clear();
+                        }
+                        else if (IsLetter(c))
+                        {
+                            //example (myVar)
+                            state = EState.Id;
+                            sb.Append(c);
+                        }
+                        else if (IsSymbol(c))
+                        {
+                            //<= already taken into consideration. Cannot happen here. This will be a new symbol
+                            sb.Append(c);
+                            Symbol token = new Symbol(sb.ToString(), state);
+                            tokens.Add(token);
+                            sb.Clear();
+                        }
+                        else if (IsQuotation(c))
+                        {
+                            //example ("hello")
+                            state = EState.String;
+                            sb.Append(c);
+                        }
+
+                        break;
+                    case EState.Num:
                         if (IsWhiteSpace(c))
                         {
                             Symbol token = new Symbol(sb.ToString(), state);
@@ -49,72 +89,112 @@ namespace Compiler
                             state = EState.Space;
                             sb.Clear();
                         }
-                        else if (IsLetter(c))
-                        {
-                            
-                        }
                         else if (IsSymbol(c))
                         {
-                        }
-                        else if (IsQuotation(c))
-                        {
-                        }
-
-                        break;
-                    case EState.Num:
-                        if (IsWhiteSpace(c))
-                        {
-                        }
-                        else if (IsSymbol(c))
-                        {
+                            //symbol can't be in a number unless it's a '.'
+                            if (c == '.')
+                            {
+                                sb.Append(c);
+                            }
+                            else
+                            {
+                                Symbol token = new Symbol(sb.ToString(), state);
+                                tokens.Add(token);
+                                sb.Clear();
+                                sb.Append(c);
+                                state = EState.Symbol;
+                            }
                         }
                         else if (IsLetter(c))
                         {
+                            //The only thing I can think of for valid num is a float like 3.2f, but we aren't using those so meh
+                            Symbol token = new Symbol(sb.ToString(), state);
+                            tokens.Add(token);
+                            sb.Clear();
+                            //save it as an id even though it makes no sense? idk...
+                            sb.Append(c);
+                            state = EState.Id;
                         }
                         else if (IsQuotation(c))
                         {
+                            //this one also makes no sense 1"abvc"?
+                            Symbol token = new Symbol(sb.ToString(), state);
+                            tokens.Add(token);
+                            sb.Clear();
+                            //save it as a string even though it makes no sense? idk...
+                            sb.Append(c);
+                            state = EState.Id;
                         }
                         else if (IsNum(c))
                         {
-
+                            sb.Append(c);
                         }
                         break;
                     case EState.String:
                         if (IsWhiteSpace(c))
                         {
-                            //do nothing
-                        }
-                        else if (IsSymbol(c))
-                        {
-                            
-                        }
-                        else if (IsLetter(c))
-                        {
+                            //spaces are ok when in a string
+                            sb.Append(c);
                         }
                         else if (IsQuotation(c))
                         {
+                            sb.Append(c);
+                            Symbol token = new Symbol(sb.ToString(), state);
+                            tokens.Add(token);
+                            sb.Clear();
+                            state = EState.Space;
+                        }
+                        else if (IsSymbol(c))
+                        {
+                            sb.Append(c);
+                        }
+                        else if (IsLetter(c))
+                        {
+                            sb.Append(c);
                         }
                         else if (IsNum(c))
                         {
-
+                            sb.Append(c);
                         }
                         break;
                     case EState.Id:
                         if (IsWhiteSpace(c))
                         {
+                            Symbol token = new Symbol(sb.ToString(), state);
+                            tokens.Add(token);
+                            sb.Clear();
+                            state = EState.Space;
                         }
                         else if (IsSymbol(c))
                         {
+                            //example abc_de
+                            if (c == '_')
+                            {
+                                sb.Append(c);
+                            }
+                            else
+                            {
+                                //otherwise it's a new symbol. Example: (abc);
+                                Symbol token = new Symbol(sb.ToString(), state);
+                                tokens.Add(token);
+                                sb.Clear();
+                                //Need to think of a better way to handle these symbols.
+                                //For now. Hax
+                                i--;
+                                state = EState.Space;
+                            }
                         }
                         else if (IsLetter(c))
                         {
+                            sb.Append(c);
                         }
                         else if (IsQuotation(c))
                         {
+                            //abc"db and abc "hello" does not work parse error?
                         }
                         else if (IsNum(c))
                         {
-
+                            sb.Append(c);
                         }
                         break;
                 }
